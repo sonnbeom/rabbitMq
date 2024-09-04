ccddd7354cdf0756ddc638229da129346552d7a9
soony <dhsmf4245@gmail.com>
2024-09-04 20:13:09 +0900

    2024 09 04 TIL

---
+++ src/main/java/rabbitMq/member/Member.java
@@ -0,0 +1,8 @@
+package rabbitMq.member;
+
+public class Member {
+    private String name;
+    private int age;
+    private String email;
+    private String password; // 바인딩은 생략
+}
---
+++ src/main/java/rabbitMq/message/service/MessageService.java
@@ -0,0 +1,4 @@
+package rabbitMq.message.service;
+
+public interface MessageService {
+}
