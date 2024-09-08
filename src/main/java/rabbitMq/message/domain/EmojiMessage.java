72c63be5c8b2f059c5f9c2589ab3116b9cc41297
soony <dhsmf4245@gmail.com>
2024-09-08 10:23:39 +0900

    2024 09 08 TIL

--- src/main/java/rabbitMq/message/domain/EmojiMessage.java
+++ src/main/java/rabbitMq/message/domain/EmojiMessage.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 
 public class EmojiMessage extends BaseMessageEntity{
     private String emojiCode;
+
     @Builder
     public EmojiMessage(String message, LocalDateTime sendTime, LocalDateTime receiveTime, boolean success, Member member, String emojiCode) {
         super(message, sendTime, receiveTime, success, member);
--- src/main/java/rabbitMq/message/domain/GifticonMessage.java
+++ src/main/java/rabbitMq/message/domain/GifticonMessage.java
@@ -1,5 +1,6 @@
 package rabbitMq.message.domain;
 
+import lombok.Builder;
 import rabbitMq.member.Member;
 
 import java.time.LocalDateTime;
@@ -8,6 +9,7 @@ public class GifticonMessage extends BaseMessageEntity{
     private String couponCode;
     private String imageUrl;
 
+    @Builder
     public GifticonMessage(String message, LocalDateTime sendTime, LocalDateTime receiveTime, boolean success, Member member, String couponCode, String imageUrl) {
         super(message, sendTime, receiveTime, success, member);
         this.couponCode = couponCode;
